3c6f271c64237283be50653649ee064625cc3ef5
Hugues Blanco <contact@huguesblanco.com>
2025-02-03 15:29:38 +0100

    Set up ESLint
    - Configured TypeScript-ESLint with type-checked settings
    - Added eslint-plugin-react
    - Integrated eslint-config-prettier to prevent conflicts with Prettier
    - Enforced stricter linting rules
    - Updated main.tsx to comply with new ESLint rules
    - Removed Vite example content from App component due to lint errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,90 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
+
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
+    settings: { react: { version: '18.3' } },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'function-declaration',
+        },
+      ],
+      'react/destructuring-assignment': [
+        'error',
+        'always',
+        { destructureInSignature: 'always' },
+      ],
+
       ...reactHooks.configs.recommended.rules,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+
+      eqeqeq: 'error',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'no-implicit-coercion': 'error',
+      'no-implicit-globals': 'error',
+      'no-unused-expressions': 'error',
+      camelcase: 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/strict-boolean-expressions': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     },
   },
-)
+
+  {
+    files: ['**/*.tsx'],
+    rules: {
+      'no-restricted-exports': [
+        'error',
+        {
+          restrictDefaultExports: {
+            direct: false,
+            named: true,
+            defaultFrom: true,
+            namedFrom: true,
+            namespaceFrom: true,
+          },
+        },
+      ],
+    },
+  },
+
+  eslintConfigPrettier, // Ensure this plugin is the last one in the list to disable any previous rules that conflict with the Prettier formatter.
+);
