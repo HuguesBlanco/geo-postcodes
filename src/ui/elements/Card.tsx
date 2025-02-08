bd40771f5d460bdaeda44e4ed908229ae2f39574
Hugues Blanco <contact@huguesblanco.com>
2025-02-09 00:15:21 +0100

    Place backgrounds in home cards

--- src/ui/elements/Card.tsx
+++ src/ui/elements/Card.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { LinkDatum } from '../../types/linksTypes';
 
-type CardProps = {
-  link: LinkDatum;
-  backgroundImagePath: string;
+export type CardBackgroundInfo = {
+  imagePath: string;
+  tailwindClasses: string;
 };
 
-function Card({ link, backgroundImagePath }: CardProps): React.JSX.Element {
+type CardProps = {
+  link: LinkDatum;
+  backgroundInfo: CardBackgroundInfo | null;
+};
+
+function Card({ link, backgroundInfo }: CardProps): React.JSX.Element {
   return (
     <div
       onClick={link.visit}
-      className="cursor-pointer rounded-lg shadow-md p-6 hover:shadow-lg transition-all w-96 h-36 bg-blue-100 bg-no-repeat bg-contain bg-right-bottom"
-      style={{ backgroundImage: `url(${backgroundImagePath})` }}
+      className={`cursor-pointer rounded-lg shadow-md p-6 hover:shadow-lg transition-all w-96 h-36 bg-blue-100 bg-no-repeat ${
+        backgroundInfo?.tailwindClasses ?? ''
+      }`}
+      style={
+        backgroundInfo
+          ? { backgroundImage: `url(${backgroundInfo.imagePath})` }
+          : {}
+      }
     >
       <p className="font-black text-blue-900">{link.label}</p>
     </div>
--- src/ui/layouts/HomeLayout.tsx
+++ src/ui/layouts/HomeLayout.tsx
@@ -5,26 +5,37 @@ import knowledgeBaseImagePath from '../../assets/kb.svg';
 import backgroundImagePath from '../../assets/main_background.webp';
 import mapExplorerImagePath from '../../assets/map_explorer.webp';
 import { LinksData } from '../../types/linksTypes';
-import Card from '../elements/Card';
+import Card, { CardBackgroundInfo } from '../elements/Card';
 
 type HomeLayoutProps = {
   links: LinksData;
 };
 
 function HomeLayout({ links }: HomeLayoutProps): React.JSX.Element {
-  const cardBackgroundPaths = [
-    dataExplorerImagePath,
-    mapExplorerImagePath,
-    downloadCenterImagePath,
-    knowledgeBaseImagePath,
+  const cardBackgrounds: CardBackgroundInfo[] = [
+    {
+      imagePath: dataExplorerImagePath,
+      tailwindClasses: 'bg-[25rem_auto] bg-[right_-5rem_bottom_-5rem]',
+    },
+    {
+      imagePath: mapExplorerImagePath,
+      tailwindClasses: 'bg-[15rem_auto] bg-[right_0rem_bottom_0rem]',
+    },
+    {
+      imagePath: downloadCenterImagePath,
+      tailwindClasses: 'bg-[6rem_auto] bg-[right_3rem_bottom_1.5rem]',
+    },
+    {
+      imagePath: knowledgeBaseImagePath,
+      tailwindClasses: 'bg-[7rem_auto] bg-[right_2rem_bottom_2rem]',
+    },
   ];
+
   return (
-    <div className="flex-1 bg-cover bg-center bg-no-repeat flex flex-col">
-      <div>
-        <h1 className="text-center my-24 text-2xl font-black text-blue-900">
-          Welcome to GeoPostcodes&apos; Customer Portal
-        </h1>
-      </div>
+    <div className="flex flex-col flex-1 bg-cover bg-center bg-no-repeat">
+      <h1 className="text-center my-24 text-2xl font-black text-blue-900">
+        Welcome to GeoPostcodes&apos; Customer Portal
+      </h1>
 
       <div className="max-w-4xl mx-auto px-4">
         <div className="grid grid-cols-2 gap-8 justify-items-center">
@@ -32,9 +43,7 @@ function HomeLayout({ links }: HomeLayoutProps): React.JSX.Element {
             <Card
               key={link.path}
               link={link}
-              backgroundImagePath={
-                cardBackgroundPaths[index] ?? dataExplorerImagePath
-              }
+              backgroundInfo={cardBackgrounds[index] ?? null}
             />
           ))}
         </div>
@@ -43,7 +52,7 @@ function HomeLayout({ links }: HomeLayoutProps): React.JSX.Element {
       <div
         className="flex-1 bg-cover bg-top bg-no-repeat"
         style={{ backgroundImage: `url(${backgroundImagePath})` }}
-      ></div>
+      />
     </div>
   );
 }
